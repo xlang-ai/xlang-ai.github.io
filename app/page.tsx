95abd1da40f1cca72a3a83bc2d4d8ee7f1d5de0a
Jing Hua <tohjinghua123@gmail.com>
2023-06-07 22:49:10 +0800

    demo section

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import Demo from './components/Demo/Demo';
 import Hero from './components/Hero/Hero';
 
 const Home = () => {
   return (
     <div>
       <Hero />
+      <Demo />
     </div>
   );
 };

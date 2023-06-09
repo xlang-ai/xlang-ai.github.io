4775ff5dc37d09ed861a80a72895dfc5304a3b19
Jing Qiang <tohjingqiang@gmail.com>
2023-06-09 19:16:34 +0800

    implement demo video section

--- app/components/Demo/Demo.tsx
+++ app/components/Demo/Demo.tsx
@@ -7,8 +7,11 @@ const Demo = () => {
     <div className="flex flex-col items-center justify-center px-6 py-10">
       <h2 className="title">Demo</h2>
       <div className="w-full max-w-[80%]">
-        <div className="rounded-lg bg-gray-200 dark:bg-gray-800 w-full h-60 mt-8 flex items-center justify-center">
-          Video
+        {/* Demo Video */}
+        <div className="w-full h-auto mt-8">
+          <video className="w-full h-full" controls>
+            <source src="xlang-chat-demo.mp4" type="video/mp4" />
+          </video>
         </div>
 
         {/* Live Demo Button */}

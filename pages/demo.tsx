34f2c8e386dac8d77d791c47434d147f85f99518
Jing Hua <59118459+ztjhz@users.noreply.github.com>
2023-07-08 09:16:20 -0700

    Update demo.tsx

---
+++ pages/demo.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const demo = () => {
+  return (
+    <div className='py-20'>
+      <h1 className='text-center'>Coming soon...</h1>
+    </div>
+  );
+};
+
+export default demo;

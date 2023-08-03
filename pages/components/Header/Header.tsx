0b7e71941f26f4edd28f82ab794018aea70a0738
Jing Hua <tohjinghua123@gmail.com>
2023-08-03 10:34:30 -0700

    update header

--- pages/components/Header/Header.tsx
+++ pages/components/Header/Header.tsx
@@ -10,109 +10,73 @@ const Header = () => {
   const [showMenu, setShowMenu] = useState<boolean>(false);
 
   return (
-    <div className='fixed top-0 left-0 w-full h-14 md:h-20 bg-brand-offWhite py-4 px-6 flex justify-between items-center z-10'>
-      <Link href='/'>
-        <div className='flex gap-2 items-center cursor-pointer text-brand-dark'>
-          <Image
-            src={publicFilePath('/logo.svg')}
-            alt='Xlang'
-            width={30}
-            height={30}
-            className='rounded-md'
-          />
-          <div>XLANG</div>
+    <div className='fixed top-0 left-0 w-full h-14 md:h-20 bg-brand-offWhite py-4 z-10'>
+      <div className='page-x-width w-full flex justify-between items-center'>
+        <Link href='/'>
+          <div className='flex gap-2 items-center cursor-pointer text-brand-dark'>
+            <Image
+              src={publicFilePath('/logo.svg')}
+              alt='Xlang'
+              width={30}
+              height={30}
+              className='rounded-md'
+            />
+            <div>XLANG Lab</div>
+          </div>
+        </Link>
+        <div className='sm:hidden' onClick={() => setShowMenu(true)}>
+          <Menu2 />
         </div>
-      </Link>
-      <div className='sm:hidden' onClick={() => setShowMenu(true)}>
-        <Menu2 />
-      </div>
-      {showMenu && <SideMenu setShowMenu={setShowMenu} />}
-      <ul className='gap-8 text-md text-text-brand-dark hidden sm:flex'>
-        <li className='font-medium hover:underline'>
-          <Link href='/about'>About</Link>
-        </li>
-        <li className='font-medium hover:underline'>
-          <Link href='/team'>Team</Link>
-        </li>
-        <li className='font-medium hover:underline'>
-          <Link href='/research'>Research</Link>
-        </li>
-        <li className='font-medium hover:underline'>
-          <Link href='https://chat.xlang.ai'>Demos</Link>
-        </li>
-        <li className='font-medium hover:underline'>
-          <Link href='/blog'>Blogs</Link>
-        </li>
-      </ul>
+        {showMenu && <SideMenu setShowMenu={setShowMenu} />}
+        <ul className='gap-8 text-md text-text-brand-dark hidden sm:flex'>
+          <li className='font-medium hover:underline text-brand-dark'>
+            <Link href='/about'>about</Link>
+          </li>
+          <li className='font-medium hover:underline text-brand-dark'>
+            <Link href='/team'>team</Link>
+          </li>
+          <li className='font-medium hover:underline text-brand-dark'>
+            <Link href='/research'>research</Link>
+          </li>
+          <li className='font-medium hover:underline text-brand-dark'>
+            <Link href='https://chat.xlang.ai'>demos</Link>
+          </li>
+          <li className='font-medium hover:underline text-brand-dark'>
+            <Link href='/blog'>blogs</Link>
+          </li>
+        </ul>
 
-      <ul className='hidden lg:flex gap-6'>
-        <li>
-          <Link href='mailto:mail@xlang.ai'>
-            <Image
-              src={publicFilePath('/mail.svg')}
-              alt='Xlang'
-              width={25}
-              height={25}
-              className='rounded-md'
-            />
-          </Link>
-        </li>
-        <li>
-          <Link href='mailto:mail@xlang.ai'>
-            <Image
-              src={publicFilePath('/slack.svg')}
-              alt='Xlang'
-              width={25}
-              height={25}
-              className='rounded-md'
-            />
-          </Link>
-        </li>
-        <li>
-          <Link href='mailto:mail@xlang.ai'>
-            <Image
-              src={publicFilePath('/discord.svg')}
-              alt='Xlang'
-              width={25}
-              height={25}
-              className='rounded-md'
-            />
-          </Link>
-        </li>
-        <li>
-          <Link href='mailto:mail@xlang.ai'>
-            <Image
-              src={publicFilePath('/github.svg')}
-              alt='Xlang'
-              width={25}
-              height={25}
-              className='rounded-md'
-            />
-          </Link>
-        </li>
-        <li>
-          <Link href='mailto:mail@xlang.ai'>
-            <Image
-              src={publicFilePath('/twitter.svg')}
-              alt='Xlang'
-              width={25}
-              height={25}
-              className='rounded-md'
-            />
-          </Link>
-        </li>
-        <li>
-          <Link href='mailto:mail@xlang.ai'>
-            <Image
-              src={publicFilePath('/threads.svg')}
-              alt='Xlang'
-              width={25}
-              height={25}
-              className='rounded-md'
-            />
-          </Link>
-        </li>
-      </ul>
+        <div className='flex gap-4 items-center'>
+          <ul className='hidden lg:flex gap-3'>
+            <li>
+              <Link href='mailto:mail@xlang.ai'>
+                <Image
+                  src={publicFilePath('/github.svg')}
+                  alt='Xlang'
+                  width={25}
+                  height={25}
+                  className='rounded-md'
+                />
+              </Link>
+            </li>
+            <li>
+              <Link href='mailto:mail@xlang.ai'>
+                <Image
+                  src={publicFilePath('/twitter.svg')}
+                  alt='Xlang'
+                  width={25}
+                  height={25}
+                  className='rounded-md'
+                />
+              </Link>
+            </li>
+          </ul>
+
+          <div className='border border-brand-primary2 border-2 text-brand-primary2 font-[500] rounded-xl py-1 px-3 cursor-pointer'>
+            join us
+          </div>
+        </div>
+      </div>
     </div>
   );
 };

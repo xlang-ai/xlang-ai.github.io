85757c648fa39e0a69610e0c2419ac1661c440b5
Jing Hua <tohjinghua123@gmail.com>
2023-08-03 17:48:55 -0700

    news section

---
+++ pages/components/News/News.tsx
@@ -0,0 +1,71 @@
+import React from 'react';
+
+interface NewsInterface {
+  date: Date;
+  description: string;
+}
+const news: NewsInterface[] = [
+  {
+    date: new Date('2021-10-26'),
+    description: 'The (renewed) group website is now live!',
+  },
+  {
+    date: new Date('2021-10-26'),
+    description: 'The (renewed) group website is now live!',
+  },
+  {
+    date: new Date('2021-10-26'),
+    description: 'The (renewed) group website is now live!',
+  },
+  {
+    date: new Date('2021-10-26'),
+    description: 'The (renewed) group website is now live!',
+  },
+  {
+    date: new Date('2021-10-26'),
+    description: 'The (renewed) group website is now live!',
+  },
+];
+
+const parseDate = (date: Date) => {
+  const months = [
+    'Jan',
+    'Feb',
+    'Mar',
+    'Apr',
+    'May',
+    'Jun',
+    'Jul',
+    'Aug',
+    'Sep',
+    'Oct',
+    'Nov',
+    'Dec',
+  ];
+
+  const month = months[date.getUTCMonth()];
+  const day = date.getUTCDate();
+  const year = date.getUTCFullYear();
+
+  return `${month} ${day}, ${year}`;
+};
+
+const News = () => {
+  return (
+    <div className='w-full bg-[#D9D9D9]/20 py-16'>
+      <div className='page-x-width'>
+        <h1 className='text-2xl font-normal'>News</h1>
+        <div className='flex flex-col gap-2'>
+          {news.map((n) => (
+            <div className='flex gap-8 flex-wrap py-3 border-b border-black/30 text-sm'>
+              <div>{parseDate(n.date)}</div>
+              <div>{n.description}</div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default News;

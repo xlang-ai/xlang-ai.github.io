07162f1a5b81260f9da79a33756a607c2658cc8d
Jing Hua <tohjinghua123@gmail.com>
2023-08-03 17:48:55 -0700

    news section

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,11 +2,13 @@ import Highlight from './components/Highlight';
 import ResearchDirections from './components/ResearchDirections';
 import Acknowledgments from './components/Acknowledgement';
 import Welcome from './components/Welcome';
+import News from './components/News';
 
 const Home = () => {
   return (
     <div>
       <Welcome />
+      <News />
       <ResearchDirections />
       <Highlight />
     </div>

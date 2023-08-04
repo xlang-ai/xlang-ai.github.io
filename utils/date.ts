c97ffe2c7a02389e9be23b7a29e4868e165716e9
Jing Hua <tohjinghua123@gmail.com>
2023-08-04 11:12:23 -0700

    refactor parseDate

---
+++ utils/date.ts
@@ -0,0 +1,22 @@
+export const parseDate = (date: Date) => {
+  const months = [
+    'Jan',
+    'Feb',
+    'Mar',
+    'Apr',
+    'May',
+    'Jun',
+    'Jul',
+    'Aug',
+    'Sep',
+    'Oct',
+    'Nov',
+    'Dec',
+  ];
+
+  const month = months[date.getUTCMonth()];
+  const day = date.getUTCDate();
+  const year = date.getUTCFullYear();
+
+  return `${month} ${day}, ${year}`;
+};

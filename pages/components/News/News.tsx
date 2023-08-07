bec2bf4426027255f1411798fab8b3912df04d2f
Jing Hua <tohjinghua123@gmail.com>
2023-08-07 14:35:25 -0700

    handle no news

--- pages/components/News/News.tsx
+++ pages/components/News/News.tsx
@@ -2,21 +2,22 @@ import React from 'react';
 import { parseDateString } from '@/utils/date';
 import { News as NewsInterface } from '@/interface/news';
 
-const News = ({ news }: { news: NewsInterface[] }) => {
+const News = ({ news }: { news?: NewsInterface[] }) => {
   return (
     <div className='w-full bg-[#D9D9D9]/20 max-sm:py-4 py-16'>
       <div className='page-x-width'>
         <h1 className='text-2xl font-normal'>News</h1>
         <div className='flex flex-col gap-2'>
-          {news.map((n) => (
-            <div
-              className='flex gap-x-8 flex-wrap py-3 border-b border-black/30 text-sm'
-              key={n.description}
-            >
-              <div>{parseDateString(n.date)}</div>
-              <div>{n.description}</div>
-            </div>
-          ))}
+          {news &&
+            news.map((n) => (
+              <div
+                className='flex gap-x-8 flex-wrap py-3 border-b border-black/30 text-sm'
+                key={n.description}
+              >
+                <div>{parseDateString(n.date)}</div>
+                <div>{n.description}</div>
+              </div>
+            ))}
         </div>
       </div>
     </div>

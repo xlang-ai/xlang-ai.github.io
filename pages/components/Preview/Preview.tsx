55bdb19b4526c2e056858999fe3c4c5e53bf3638
Jing Hua <tohjinghua123@gmail.com>
2023-08-07 15:07:11 -0700

    refactor preview

--- pages/components/Preview/Preview.tsx
+++ pages/components/Preview/Preview.tsx
@@ -2,12 +2,11 @@ import Image, { StaticImageData } from 'next/image';
 import React, { useState } from 'react';
 
 import DemoImage from '@/public/demo/demo.png';
-import DataAgentImage from '@/public/icons/data-agent.png';
-import PluginImage from '@/public/icons/plugin.svg';
-import WebAgentImage from '@/public/icons/web-agent.svg';
+import { HighlightSubProject } from '@/interface/project';
+import Link from 'next/link';
 
-const Preview = () => {
-  const [preview, setPreview] = useState<string | StaticImageData>(DemoImage);
+const Preview = ({ subProj }: { subProj?: HighlightSubProject[] }) => {
+  const [index, setIndex] = useState<number>(0);
 
   return (
     <div className='w-full max-sm:py-8 py-16'>
@@ -19,108 +18,88 @@ const Preview = () => {
               Open-source framework and ecosystem for building and evaluating
               LLM-based agents
             </div>
-            <div
-              className='max-sm:w-full btn btn-primary text-xs font-[600] py-2'
-              onClick={() => window.open('https://chat.xlang.ai', '_blank')}
-            >
-              Start Chatting
+            <Link href='https://chat.xlang.ai' target='_blank'>
+              <div
+                className='max-sm:w-full btn btn-primary text-xs font-[600] py-2'
+                onClick={() => window.open('https://chat.xlang.ai', '_blank')}
+              >
+                Start Chatting
+              </div>
+            </Link>
+          </div>
+          {subProj && (
+            <div className='max-sm:col-span-4 max-sm:h-[300px] max-sm:my-2 rounded-lg overflow-hidden col-span-3 relative'>
+              <Image
+                src={subProj[0].image}
+                alt='Demo'
+                fill
+                style={{ objectFit: 'contain', objectPosition: 'left center' }}
+              />
             </div>
-          </div>
-          <div className='max-sm:col-span-4 max-sm:h-[300px] max-sm:my-2 rounded-lg overflow-hidden col-span-3 relative'>
-            <Image
-              src={preview}
-              alt='Demo'
-              fill
-              style={{ objectFit: 'contain', objectPosition: 'left center' }}
-            />
-          </div>
+          )}
         </div>
 
         <div className='flex flex-wrap gap-10 justify-center'>
-          {features.map((feature) => (
-            <FeatureBox
-              feature={feature}
-              preview={preview}
-              setPreview={setPreview}
-              key={feature.title}
-            />
-          ))}
+          {subProj &&
+            subProj.map((proj, i) => (
+              <FeatureBox
+                proj={proj}
+                selected={index === i}
+                index={i}
+                setIndex={setIndex}
+                key={proj.title}
+              />
+            ))}
         </div>
       </div>
     </div>
   );
 };
 
-interface Feature {
-  icon: string | StaticImageData;
-  title: string;
-  desc: string;
-  video?: string | StaticImageData;
-  details?: string;
-}
-
-const features: Feature[] = [
-  {
-    icon: DataAgentImage,
-    title: 'Data Agent',
-    desc: 'open-source version of OpenAI with LLM + more data tools',
-    video: DemoImage,
-    details: '1',
-  },
-  {
-    icon: PluginImage,
-    title: 'Plugins',
-    desc: 'open-source version of OpenAI by manipulating webs/plugins via APIs - web/app apis',
-    video: '/demo/xlang-demo.png',
-    details: '2',
-  },
-  {
-    icon: WebAgentImage,
-    title: 'Web Agent',
-    desc: 'grounding natural language instructions into click and typing actions to manipulate webs as humans do.',
-    video: '/icons/data-agent.svg',
-    details: '3',
-  },
-];
-
 const FeatureBox = ({
-  feature,
-  preview,
-  setPreview,
+  proj,
+  selected,
+  index,
+  setIndex,
 }: {
-  feature: Feature;
-  preview: string | StaticImageData;
-  setPreview: React.Dispatch<React.SetStateAction<string | StaticImageData>>;
+  proj: HighlightSubProject;
+  selected: boolean;
+  index: number;
+  setIndex: React.Dispatch<React.SetStateAction<number>>;
 }) => {
-  const isSelected = preview === feature.video;
-
   return (
     <div
       className='flex flex-col items-center shadow-lg rounded-2xl p-6 max-w-[220px] border cursor-pointer'
-      style={isSelected ? { borderColor: '#0156AC' } : {}}
-      onClick={() => setPreview(feature.video)}
+      style={selected ? { borderColor: '#0156AC' } : {}}
+      onClick={() => setIndex(index)}
     >
       <div className='relative w-12 h-12'>
         <Image
-          src={feature.icon}
-          alt={feature.title}
+          src={proj.icon}
+          alt={proj.title}
           fill
           style={{ objectFit: 'contain', objectPosition: 'center' }}
         />
       </div>
-      <div className='text-lg'>{feature.title}</div>
-      <div className='text-[10px] text-center flex-1 pb-4'>{feature.desc}</div>
+      <div className='text-lg'>{proj.title}</div>
+      <div className='text-[10px] text-center flex-1 pb-4'>
+        {proj.shortDesc}
+      </div>
 
       <div className='flex gap-4'>
-        {feature.video && (
-          <div className='btn cursor-pointer text-[#0156AC] border border-[#0156AC] font-[600] rounded-lg py-1 px-4 text-xs'>
-            Video
-          </div>
+        {proj.videoLink && (
+          <Link href={proj.videoLink} target='_blank'>
+            <div className='btn cursor-pointer text-[#0156AC] border border-[#0156AC] font-[600] rounded-lg py-1 px-4 text-xs'>
+              Video
+            </div>
+          </Link>
         )}
-        {feature.details && (
-          <div className='btn cursor-pointer bg-[#0156AC] text-white border border-[#0156AC] font-[600] rounded-lg py-1 px-4 text-xs'>
-            Details
-          </div>
+        {proj.slidesLink && (
+          <Link href={proj.slidesLink} target='_blank'>
+            <div className='btn cursor-pointer bg-[#0156AC] text-white border border-[#0156AC] font-[600] rounded-lg py-1 px-4 text-xs'>
+              Details
+            </div>
+          </Link>
         )}
       </div>
     </div>

7952422df2c831bbe9fe66aabb57aca25c914aa8
Jing Hua <tohjinghua123@gmail.com>
2023-08-07 12:16:58 -0700

    refactor socials

---
+++ data/socials/socials.ts
@@ -0,0 +1,6 @@
+export const email = 'mail@xlang.ai';
+export const slack = 'https://github.com/xlang-ai';
+export const discord = 'https://github.com/xlang-ai';
+export const github = 'https://github.com/xlang-ai';
+export const twitter = 'https://github.com/xlang-ai';
+export const threads = 'https://github.com/xlang-ai';

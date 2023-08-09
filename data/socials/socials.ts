c09bed83f15912470785f36ba8ba08e15930df49
koalazf99 <2275008634@qq.com>
2023-08-10 05:05:29 +0800

    add discord & slack

--- data/socials/socials.ts
+++ data/socials/socials.ts
@@ -1,8 +1,8 @@
 export const email = 'mail@xlang.ai';
 //TODO
-export const slack = 'https://github.com/xlang-ai';
+export const slack = 'https://join.slack.com/t/xlanggroup/shared_invite/zt-20zb8hxas-eKSGJrbzHiPmrADCDX3_rQ';
 //TODO
-export const discord = 'https://github.com/xlang-ai';
+export const discord = 'https://discord.gg/sNURNAQs';
 //TODO
 export const github = 'https://github.com/xlang-ai';
 export const twitter = 'https://twitter.com/XLangAILabs';

7d746dc258db53fe05a7673eab1af2d643f199cc
Tianbao Xie <47296835+Timothyxxx@users.noreply.github.com>
2023-09-08 13:43:24 +0800

    Update discord (#36)
    * Update discord
    
    * Update README.md
    
    ---------
    
    Co-authored-by: Fan <37338733+koalazf99@users.noreply.github.com>

--- data/socials/socials.ts
+++ data/socials/socials.ts
@@ -1,6 +1,6 @@
 export const email = 'mail@xlang.ai';
 export const slack = 'https://join.slack.com/t/xlanggroup/shared_invite/zt-20zb8hxas-eKSGJrbzHiPmrADCDX3_rQ';
-export const discord = 'https://discord.gg/sNURNAQs';
+export const discord = 'https://discord.com/invite/4Gnw7eTEZR';
 export const github = 'https://github.com/xlang-ai';
 export const twitter = 'https://twitter.com/XLangAI';
 //TODO

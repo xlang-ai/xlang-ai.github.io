4e9e7d31634e58f3e48991a1bc4adad4a9ca9997
Junhaoo-Chen <Junhaoo-Chen@users.noreply.github.com>
2026-05-26 10:36:24 +0000

    deploy: 7237875007e494121979db69f2a7056c9803beef

--- _next/static/chunks/pages/components/Footer-bbc598d80f849e7b.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[450],{2302:function(n,_,o){(window.__NEXT_P=window.__NEXT_P||[]).push(["/components/Footer",function(){return o(810)}])}},function(n){n.O(0,[774,888,179],function(){return n(n.s=2302)}),_N_E=n.O()}]);
